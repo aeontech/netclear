bcb561f07779b935ab6ac59c85753e3128e206a0
Shane Thompson <shane.thompson@aeontech.com.au>
2018-08-13 12:04:09 +0800

    Changed devices.py to make it work

--- devices/devices.py
+++ devices/devices.py
@@ -1,24 +1,28 @@
 import sys
 import importlib
 
+
 def exists(name):
     if sys.version_info[0] is not 3:
-        raise EnvironmentError('Python v%d is not supported' % sys.version_info[0])
+        raise EnvironmentError('Python v%d is not supported' %
+                               sys.version_info[0])
 
     if sys.version_info[1] < 4:
         loader = importlib.find_loader(name)
     else:
-        loader = importlib.util.find_spec(name)
+        from importlib import util
+        loader = importlib.util.find_spec(name, package="devices")
 
     return loader is not None
 
+
 def include(name):
-    importlib.import_module(name)
+    return importlib.import_module(name)
+
 
 def get(name):
-    if not exists(name):
+    if not exists("devices.%s" % name):
         raise LookupError('Device "%s" not supported' % name)
 
-    include(name)
-
-    return name()
+    module = include("devices.%s" % name)
+    return getattr(module, name.split('.')[-1])
