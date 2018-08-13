661d7ce063b71e22b6f6a2f386275125a49bbb35
Shane Thompson <shane.thompson@aeontech.com.au>
2018-08-13 12:05:57 +0800

    Starting to pack out the base device

--- devices/Base.py
+++ devices/Base.py
@@ -1,14 +1,53 @@
+import wx
+import os  # We don't really need this
+import sys
+from bits.Terminal import TerminalCtrl
+
+
 class Base:
     _comms = None
+
+    CTRL_C = '\x03'
 
     def __init__(self):
         pass
 
-    def getCommsPort():
+    def getCommsPort(self):
         return self._comms
 
-    def setCommsPort(communications):
+    def setCommsPort(self, communications):
         self._comms = communications
 
-    def run():
+    def run(self):
         raise NotImplementedError()
+
+    def setup(self):
+        classname = self.__class__.__name__
+
+        app = wx.App()
+        size = wx.Size(700, 450)
+        frame = wx.Frame(None, title="NetClear: %s" % classname, size=size)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        cmd = TerminalCtrl(frame)
+        cmd.SetSpacing(0)
+        cmd.SetWrap(True)
+
+        cmd.AddChars('A abcdefghijklmnopqrstuvwxyz  abcdefghijklmnopqrstuvwxyz'
+                     '  abcdefghijklmnopqrstuvwxyz  abcdefghijklmnopqrstuvwxyz'
+                     '  abcdefghijklmnopqrstuvwxyz  abcdefghijklmnopqrstuvwxyz'
+                     + os.linesep)
+        cmd.AddChars('B 12345678901234567890123456' + os.linesep)
+
+        frame.Bind(wx.EVT_CLOSE, lambda e: frame.Destroy() and sys.exit())
+
+        sizer.Add(cmd, 1, wx.EXPAND)
+        frame.SetSizer(sizer)
+        frame.SetMinSize(wx.Size(313, 260))
+        frame.Show()
+
+        app.MainLoop()
+
+    def myprompt(self, msg, cap="netclear"):
+        styl = wx.OK | wx.OK_DEFAULT | wx.STAY_ON_TOP | wx.ICON_HAND
+        wx.MessageDialog(message=msg, caption=cap, style=styl)
