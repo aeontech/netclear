5906f8b4e90a8ee79293750a6bfa0330fb670cc9
Shane Thompson <shane.thompson@aeontech.com.au>
2018-08-13 12:04:27 +0800

    Starting to sketch out asa55xx.py

--- devices/cisco/asa55xx.py
+++ devices/cisco/asa55xx.py
@@ -1,5 +1,14 @@
+import wx
 from ..Base import Base
 
 
 class asa55xx(Base):
-    pass
+    def run(self):
+        self.setup()
+
+        self.prompt("Power on the device then click 'OK'.")
+
+        self.serial.send_break()
+        self.serial.wait('rommon 1 >')
+        self.serial.send('confreg')
+        self.serial.wait('')
