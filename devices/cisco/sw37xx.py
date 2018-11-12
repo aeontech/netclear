c86b993fa092983055758eb9c929fcc40d9d3d75
Shane Thompson <shane.thompson@aeontech.com.au>
2018-11-12 09:13:56 +0800

    Added commentary to inherited devices

--- devices/cisco/sw37xx.py
+++ devices/cisco/sw37xx.py
@@ -2,4 +2,6 @@ from .sw35xx import sw35xx
 
 
 class sw37xx(sw35xx):
+    # This is exactly the same as the 35xx procedure, from which we
+    # inherit all behaviour. We will leave this class blank.
     pass
